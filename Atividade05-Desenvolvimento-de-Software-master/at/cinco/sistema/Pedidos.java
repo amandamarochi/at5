c1f21c1649ce6fddd703bb66320b69c636a7fa94
Amanda Marochi <62608463+amandamarochi@users.noreply.github.com>
2020-09-28 21:42:04 -0300

    Update Pedidos.java

--- Atividade05-Desenvolvimento-de-Software-master/at/cinco/sistema/Pedidos.java
+++ Atividade05-Desenvolvimento-de-Software-master/at/cinco/sistema/Pedidos.java
@@ -1,7 +1,4 @@
-//Fazer a parte de processamento de pedidos 
-
 package at.cinco.sistema;
-
 import at.cinco.dominio.Cardapio;
 import at.cinco.dominio.Item;
 
@@ -12,20 +9,17 @@ import java.util.Scanner;
 
 public class Pedidos {
 
-    static int codigoPedido = 1;
     static List<String> listaDePedidos = new ArrayList<>();
 
-    public static void novoPedido() throws IOException {
-        FileWriter arquivo = new FileWriter("C:\\AT4REMASTERED\\pedido " + codigoPedido + ".txt");
+    public static void novo() throws IOException {
+        FileWriter arquivo = new FileWriter("C:\\AT4REMASTERED\\pedido.txt");
         PrintWriter gravador = new PrintWriter(arquivo);
-        gravador.println("PEDIDO " + codigoPedido + "\n");
+        gravador.println("PEDIDO " + "\n");
         pedido(gravador);
-        codigoPedido++;
     }
 
-    public static void pedido(PrintWriter gravador) throws FileNotFoundException {
+    public static void pedido(PrintWriter gravador){
         Scanner leitor = new Scanner(System.in);
-        int cod;
 
         System.out.println("DESEJA FAZER UM PEDIDO?");
 
@@ -40,7 +34,6 @@ public class Pedidos {
             //Item itemAdd2 = Cardapio.buscarPorNomeBebidas(leitor.nextLine());
             //Item itemAdd3 = Cardapio.buscarPorNomeVinhos(leitor.nextLine());
 
-
             System.out.println("Adicionar observacao: ");
             String obs = leitor.nextLine();
 
@@ -50,52 +43,34 @@ public class Pedidos {
                 //gravador.println("Nome: " + itemAdd3.getNome() + " Preco:" + itemAdd3.getPreco() + " Obs: " + obs);
                 System.out.println("Nome: " + itemAdd.getNome() + " Preco:" + itemAdd.getPreco() + " Obs: " + obs);
             } else {
-                gravador.println(itemAdd.getNome() + itemAdd.getPreco());
-                System.out.println("Nome: " + itemAdd.getNome() + " Preco:" + itemAdd.getPreco());
+                gravador.println("Nome: " + itemAdd.getNome() + " Preco:" + itemAdd.getPreco() + "\n");
             }
         System.out.println("DESEJA REALIZAR NOVO PEDIDO?");
         opcao = leitor.next().charAt(0);
-
     }
-
         gravador.close();
     }
 
-    public static void pedidoExistente() throws IOException {
 
-        listaDePedidos.clear();
+
+    public static void pedir() throws IOException {
+
         Scanner leitor = new Scanner(System.in);
-
-        System.out.println("Informe o codigo: ");
-        leitor.nextInt();
-        File arquivo = new File("C:\\AT4REMASTERED\\pedido " + codigoPedido + ".txt");
+        File arquivo = new File("C:\\AT4REMASTERED\\pedido.txt");
         leitor = new Scanner(arquivo);
-        double total = 0;
 
         while(leitor.hasNext()) {
             listaDePedidos.add(leitor.nextLine());
         }
 
-        FileWriter arquivoNovo = new FileWriter(arquivo);
+        FileWriter novo = new FileWriter(arquivo);
         PrintWriter gravador = new PrintWriter(arquivo);
 
-        int i = 0;
         for(String linha : listaDePedidos) {
-
-            if(i == listaDePedidos.size() - 2) {
-
-            }else if(linha.contains("TOTAL")) {
-                String [] partes = linha.split("\t");
-
-            }else {
                 gravador.println(linha);
-            }
-            i++;
         }
-
         pedido(gravador);
-
-        arquivoNovo.close();
+        novo.close();
     }
 
 }
